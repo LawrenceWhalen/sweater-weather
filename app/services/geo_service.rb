541d93bcf7116fb6c20bce2ddba56fcbed797277
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-08-08 17:03:50 -0600

    Add weather service

--- app/services/geo_service.rb
+++ app/services/geo_service.rb
@@ -1,7 +1,12 @@
 class GeoService
 
   def self.get_geocoding(location)
-    response = conn.get("/geocoding/v1/address?&location=#{location}&thumbMaps=false&maxResults=1&outFormat=json")
+    response = conn.get("/geocoding/v1/address") do |conn|
+      conn.params[:location] = location
+      conn.params[:thumbMaps] = false
+      conn.params[:addressmaxResults] = 1
+      conn.params[:outFormat] = 'json'
+    end
 
     parse_json(response)
   end
