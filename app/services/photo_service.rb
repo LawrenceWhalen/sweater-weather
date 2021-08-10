02e6c1315a10da030b869202db217795aab214f9
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-08-10 16:41:39 -0600

    Create photo service

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -1,8 +1,17 @@
-class AaService
-  def self..photo_by_coord(location_hash)
-    response = conn.get("/services/rest/") do |conn|
-      conn.params[:by_dist] = "#{location[:lat]},#{location[:lng]}"
-      conn.params[:per_page] = quantity
+class PhotoService
+  def self.photo_by_coord(location, city_name)
+    response = conn.get("/services/rest/?method=flickr.photos.search") do |conn|
+      conn.params[:sort] = 'relevance'
+      conn.params[:privacy_filter] = 1
+      conn.params[:accuracy] = 11
+      conn.params[:content_type] = 1
+      conn.params[:media] = 'photos'
+      conn.params[:sort] = 'relevance'
+      conn.params[:lat] = location[:lat].to_i
+      conn.params[:lon] = location[:lon].to_i
+      conn.params[:per_page] = 1
+      conn.params[:format] = 'json'
+      conn.params[:nojsoncallback] = 1
     end
 
     parse_json(response)
@@ -20,5 +29,3 @@ class AaService
     end
   end
 end
-
-?method=flickr.photos.search&api_key=&text=Texarkana&sort=relevance&privacy_filter=1&accuracy=11&safe_search=1&content_type=1&media=photos&has_geo=1&geo_context=2&lat=33.44&lon=-94.04&per_page=1&format=json&nojsoncallback=1
--- spec/service/photo_service_spec.rb
+++ spec/service/photo_service_spec.rb
@@ -6,9 +6,12 @@ RSpec.describe PhotoService do
       describe 'happy_paht' do
         it 'returns a photos information' do
           VCR.use_cassette 'photo service 1' do
-            actual = PhotoService.photo_by_coord({ lat: '33.44', lng: '-94.04' })
-            binding.pry
-            expect(actual).to eq('')
+            actual = PhotoService.photo_by_coord({ lat: '33.44', lng: '-94.04' }, 'texarkana')
+
+            expect(actual[:photos][:photo][0][:id].class).to eq(String)
+            expect(actual[:photos][:photo][0][:secret].class).to eq(String)
+            expect(actual[:photos][:photo][0][:server].class).to eq(String)
+            expect(actual[:photos][:photo][0][:owner].class).to eq(String)
           end
         end
       end
